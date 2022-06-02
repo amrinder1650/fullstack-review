a5a01bc50ec9cdf65a77fd0d764f24f138610f42
Amrinder Singh <amrinder1650@gmail.com>
2022-06-01 18:48:19 -0700

    Complete displaying top 25 repos

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const RepoList = (props) => (
-  <div>
-    <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
-  </div>
-)
+const RepoList = (props) => {
+
+  var repos = [...props.repos]
+  repos.sort((a, b) => {
+    return a.forks - b.forks;
+  }).reverse();
+  repos = repos.slice(0, 25);
+
+  return (
+    <div>
+      <h4> Repo List Component </h4>
+      There are {props.repos.length} repos.
+
+      <h4>Top 25 Repos</h4>
+
+      {repos.map(repo =>
+      <div key={repo.url}>{repo.title}, {repo.author}, {repo.forks}, {repo.url}</div>
+      )}
+
+    </div>
+  )
+}
 
 export default RepoList;
